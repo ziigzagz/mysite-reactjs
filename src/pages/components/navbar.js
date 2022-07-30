58c72faea5b408ac9954aa282a685fee6a913e23
ziigzagz <iffan.h@ku.th>
2022-07-30 20:52:15 +0700

    Initial commit

---
+++ src/pages/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <>
+        <div className="navbar">
+            <div className="nav-item">Profile</div>
+            <div className="nav-item">Home</div>
+            <div className="nav-item">Project</div>
+            <div className="nav-item"></div>
+        </div>
+        </>
+    );
+}
+
+export default Navbar;
---
+++ src/pages/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from './components/navbar';
+function Main() {
+  return (
+    <>
+    <Navbar/>
+      <div className="container">
+        <div className="row">
+          <div className="col">1g2g3</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
+
